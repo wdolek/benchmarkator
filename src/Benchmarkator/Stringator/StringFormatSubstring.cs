1b2ecd2f9c18892b0eeb45f145491f4dbbd0067c
Zdenek Havlin <wdolek@hotmail.com>
2022-08-06 23:22:13 +0200

    Add `StringFormatSubstring` benchmarks

---
+++ src/Benchmarkator/Stringator/StringFormatSubstring.cs
@@ -0,0 +1,104 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Bogus;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Benchmarkator.Stringator;
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[CategoriesColumn]
+[MemoryDiagnoser]
+public class StringFormatSubstring
+{
+    private string _data = null!;
+    private int _length;
+
+    [ParamsAllValues]
+    public Size InputSize { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        Randomizer.Seed = new Random(1337);
+        var faker = new Faker();
+
+        _data = InputSize switch
+        {
+            Size.Short => faker.Lorem.Word(),
+            Size.Lengthy => faker.Lorem.Paragraphs(3),
+            _ => throw new NotImplementedException()
+        };
+
+        _length = _data.Length / 2;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Format")]
+    public string FormatSubstring()
+    {
+        var local = _data;
+        var length = _length;
+        
+        return string.Format("{0}", local.Substring(0, length));
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Interpolated")]
+    public string FormatInterpolatedSubstring()
+    {
+        var local = _data;
+        var length = _length;
+
+        return string.Format($"{local.Substring(0, length)}");
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Format")]
+    public string FormatRange()
+    {
+        var local = _data;
+        var length = _length;
+        
+        return string.Format("{0}", local[..length]);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Interpolated")]
+    public string FormatInterpolatedRange()
+    {
+        var local = _data;
+        var length = _length;
+        
+        return string.Format($"{local[..length]}");
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Interpolated")]
+    public string FormatInterpolatedSpan()
+    {
+        var local = _data;
+        var length = _length;
+        
+        return string.Format($"{local.AsSpan(0, length)}");
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Interpolated")]
+    public string InterpolatedStringHandlerAppendFormatted()
+    {
+        var local = _data;
+        var length = _length;
+
+        var builder = new DefaultInterpolatedStringHandler(0, 1);
+        builder.AppendFormatted(local.AsSpan(0, length));
+
+        return builder.ToStringAndClear();
+    }
+
+    public enum Size
+    {
+        Short,
+        Lengthy
+    }
+}
---
+++ src/Benchmarkator/Stringator/StringFormatSubstring.md
@@ -0,0 +1,29 @@
+﻿## String.Format - what about Spans?
+
+What if we want to format string but print just part of interpolated value? `string.Substring` obviously allocates new string,
+so let's find what we can do:
+
+``` ini
+
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.19044.1826 (21H2)
+Intel Core i7-7600U CPU 2.80GHz (Kaby Lake), 1 CPU, 4 logical and 2 physical cores
+.NET SDK=6.0.302
+  [Host]     : .NET 6.0.7 (6.0.722.32202), X64 RyuJIT
+  DefaultJob : .NET 6.0.7 (6.0.722.32202), X64 RyuJIT
+
+
+```
+|                                   Method |   Categories | InputSize |      Mean |     Error |   StdDev |  Gen 0 | Allocated |
+|----------------------------------------- |------------- |---------- |----------:|----------:|---------:|-------:|----------:|
+| InterpolatedStringHandlerAppendFormatted | Interpolated |     Short |  40.11 ns |  0.197 ns | 0.153 ns | 0.0114 |      24 B |
+|              FormatInterpolatedSubstring | Interpolated |     Short |  42.66 ns |  0.339 ns | 0.301 ns | 0.0229 |      48 B |
+|                  FormatInterpolatedRange | Interpolated |     Short |  43.32 ns |  0.280 ns | 0.262 ns | 0.0229 |      48 B |
+|                          FormatSubstring |       Format |     Short |  55.54 ns |  0.663 ns | 0.621 ns | 0.0229 |      48 B |
+|                              FormatRange |       Format |     Short |  63.52 ns |  1.356 ns | 2.612 ns | 0.0229 |      48 B |
+|                   FormatInterpolatedSpan | Interpolated |     Short |  79.09 ns |  0.646 ns | 0.572 ns | 0.0229 |      48 B |
+| InterpolatedStringHandlerAppendFormatted | Interpolated |   Lengthy | 145.14 ns |  1.237 ns | 1.157 ns | 0.2983 |     624 B |
+|                          FormatSubstring |       Format |   Lengthy | 215.99 ns |  2.613 ns | 2.182 ns | 0.5968 |   1,248 B |
+|                              FormatRange |       Format |   Lengthy | 250.82 ns |  2.552 ns | 2.263 ns | 0.5965 |   1,248 B |
+|              FormatInterpolatedSubstring | Interpolated |   Lengthy | 726.12 ns |  6.602 ns | 6.175 ns | 0.5960 |   1,248 B |
+|                   FormatInterpolatedSpan | Interpolated |   Lengthy | 821.31 ns | 11.148 ns | 9.882 ns | 0.5960 |   1,248 B |
+|                  FormatInterpolatedRange | Interpolated |   Lengthy | 871.39 ns |  9.355 ns | 8.750 ns | 0.5960 |   1,248 B |
